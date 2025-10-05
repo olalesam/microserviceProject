6c4471d95ea89f2b1764c21884169227a21f0214
olalesam <olalesam87@gmail.com>
2025-10-05 13:20:45 +0000

    first commit

---
+++ department-microservice/src/test/java/com/olale/department_microservice/DepartmentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olale.department_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ teacher/src/test/java/com/olale/teacher/TeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olale.teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
