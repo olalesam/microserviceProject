a850f9657c7c2f81b79a870058822f0d294204ec
olalesam <olalesam87@gmail.com>
2025-10-10 23:34:39 +0000

    implement eurekaservice

--- teacher/src/main/java/com/olale/teacher/service/APIClient.java
+++ teacher/src/main/java/com/olale/teacher/service/APIClient.java
@@ -6,8 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-MICROSERVICE")
+//@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-MICROSERVICE")
+@FeignClient(name = "department-microservice")
 public interface APIClient {
-    @GetMapping("api/department s/{department-code}")
+    @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepByCode(@PathVariable("department-code") String departmentCode);
 }
