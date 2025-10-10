affbf3489c6658fb4328417357fcef75e9a7d072
olalesam <olalesam87@gmail.com>
2025-10-10 17:50:15 +0000

    link microservice-department and microservice-teacher using openFeign

--- teacher/src/main/java/com/olale/teacher/TeacherApplication.java
+++ teacher/src/main/java/com/olale/teacher/TeacherApplication.java
@@ -5,9 +5,11 @@ import com.olale.teacher.repo.TeacherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@EnableFeignClients
 @SpringBootApplication
 public class TeacherApplication {
 
--- teacher/src/main/java/com/olale/teacher/dto/TeacherDto.java
+++ teacher/src/main/java/com/olale/teacher/dto/TeacherDto.java
@@ -11,4 +11,5 @@ public class TeacherDto {
     private String firstName;
     private String lastName;
     private String depCode;
+    private String depName;
 }
