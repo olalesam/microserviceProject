7ab361a86327367d1e11ec60cd02c7988f949f34
olalesam <olalesam87@gmail.com>
2025-10-10 19:16:44 +0000

    bug eureka

--- teacher/src/main/java/com/olale/teacher/service/APIClient.java
+++ teacher/src/main/java/com/olale/teacher/service/APIClient.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT")
+@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-MICROSERVICE")
 public interface APIClient {
-    @GetMapping("api/departments/{department-code}")
+    @GetMapping("api/department s/{department-code}")
     DepartmentDto getDepByCode(@PathVariable("department-code") String departmentCode);
 }
